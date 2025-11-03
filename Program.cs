dc35743fda2dbe9263e8dc3faca55ab171368ba3
Muhanad-DEV <Muhanadalmoon@gmail.com>
2025-11-03 19:21:32 +0400

    chore: prepare for GitHub push; ensure docs/ ignored

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{
+    {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
